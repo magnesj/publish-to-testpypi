70f8cb37e3f5d5c7baac15fc88cc58048c213a55
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2025-05-20 06:45:38 +0200

    Dev (#1)
    * sdf
    
    * sdf
    
    * sdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * no version change
    
    * updated version number
    
    * asdf
    
    * asdf
    
    * fixes
    
    * new version
    
    * sdf
    
    * sss
    
    * asdf
    
    * v7
    
    * v8
    
    * no changes

---
+++ minimal_package/__init__.py
@@ -0,0 +1,7 @@
+"""
+A minimal example package for PyPI.
+"""
+
+from .core import greet
+
+__version__ = "0.1.0"
---
+++ minimal_package/core.py
@@ -0,0 +1,15 @@
+"""
+Core functionality for the minimal package.
+"""
+
+def greet(name="World"):
+    """
+    Return a greeting message.
+    
+    Args:
+        name (str): Name to greet. Defaults to "World".
+        
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
